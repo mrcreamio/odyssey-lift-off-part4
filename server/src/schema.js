f579986e2ce5245263fe61cb06e14e0b959d5089
ahmedtrail5 <ahmed@trailfive.com>
2022-03-28 18:17:18 +0500

    completed

--- server/src/schema.js
+++ server/src/schema.js
@@ -52,6 +52,20 @@ const typeDefs = gql`
     "The module's video url, for video-based modules"
     videoUrl: String
   }
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+  type Mutation {
+    
+  incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+}
 `;
 
 module.exports = typeDefs;
